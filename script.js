209d7ac398cb9f2c2ff8cd5637ffde233e810347
Brunbury <brunomq87@gmail.com>
2025-08-27 23:15:03 -0600

    Update script.js
    Cambio dedicatoria

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function mostrarInfo(tipo) {
     quienes: "Somos una tienda dedicada a ofrecer productos de calidad a precios accesibles.",
     mision: "Nuestra misión es brindar una experiencia de compra única y satisfactoria.",
     vision: "Ser líderes en el mercado ofreciendo innovación y servicio al cliente.",
-    dedicatoria: "Dedicado a todos los que creen que comprar bien es posible."
+    dedicatoria: "Este proyecto surge en colaboracion con mi amada futura esposa, con el proposito de tener algo muy nuestro en lo que pudieramos trabajar juntos y ayudarla llegar a ser lo grande que merece ser"
   };
 
   document.getElementById("info").innerText = info[tipo];
